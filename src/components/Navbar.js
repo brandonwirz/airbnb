dd9bb34eb5b654e9ecb3a1e8efff140a8ce8565a
Brandon Wirz <bwirz75@gmail.com>
2022-12-12 12:26:57 -0700

    adjust mobile styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from "react"
-// import logo from "../images/airbnb-logo.png"; 
+// import logo from "../images/airbnb-logo.png";
 
-import '../style.css';
+import "../style.css"
 
-export default function Navbar(){
-  return(
+export default function Navbar() {
+  return (
     <nav>
-        {/*<img src={logo} className="nav-logo" alt="airbnb logo"/>*/}
-        <img src={"./images/airbnb-logo.png"} className="nav-logo"/>
-
+      {/*<img src={logo} className="nav-logo" alt="airbnb logo"/>*/}
+      <img src={"./images/airbnb-logo.png"} className="nav-logo" />
     </nav>
   )
 }
