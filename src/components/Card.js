6511eaf5d6f07a685ea47cab303558417bf5053f
brandonwirz <bwirz75@gmail.com>
2025-04-11 12:59:02 -0600

    remove vitals

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React from "react"
 
 export default function Card(props) {
-  console.log(props.openSpots);
+  console.log(props.openSpots)
 
-  let badgeText;
+  let badgeText
   if (props.openSpots === 0) {
-    badgeText = 'SOLD OUT';
-  } else if (props.item.location === 'Online') {
-    badgeText = 'ONLINE';
-  } else if (props.item.location === 'Norway') {
-    badgeText = 'NORWAY';
-  } else if (props.item.location === 'Switzerland') {
-    badgeText = 'SWITZERLAND';
+    badgeText = "SOLD OUT"
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE"
+  } else if (props.item.location === "Norway") {
+    badgeText = "NORWAY"
+  } else if (props.item.location === "Switzerland") {
+    badgeText = "SWITZERLAND"
   }
 
   return (
     <div className="card">
       {badgeText && <div className="card-badge">{badgeText}</div>}
-      <img src={`./images/${props.item.coverImg}`} className="card-img" alt="card" />
+      <img
+        src={`./images/${props.item.coverImg}`}
+        className="card-img"
+        alt="card"
+      />
       <div className="card-stats">
-        <img src={'./images/star.png'} className="card-star" alt="star-icon" />
+        <img src={"./images/star.png"} className="card-star" alt="star-icon" />
         <span>{props.item.stats.rating}</span>
         <span className="grey">({props.item.stats.reviewCount}) •</span>
         <span className="grey">{props.item.location}</span>
@@ -29,5 +33,5 @@ export default function Card(props) {
         <span className="bold">From ${props.item.price}</span> / person
       </p>
     </div>
-  );
+  )
 }
